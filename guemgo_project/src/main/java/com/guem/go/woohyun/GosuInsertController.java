d3ff3bfe67f13cd640e259b0b8898199ad3c3812
yun <yun@LG-GRAM14ZD980-GX5BK>
2018-11-15 14:31:57 +0900

    고수 프로필 관리 페이지 버그 수정

---
+++ guemgo_project/src/main/java/com/guem/go/woohyun/GosuInsertController.java
@@ -0,0 +1,47 @@
+package com.guem.go.woohyun;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/*
+---------------------------------------------
+	2018-11-02	윤우현 파일 생성
+---------------------------------------------
+*/
+@Controller
+public class GosuInsertController {
+	@Autowired private GosuService gosuService;
+	
+	@RequestMapping(value="/GosuInsertForm",method=RequestMethod.GET)
+	public String insert() {
+		return "woohyun/gosuInsert";
+	}
+	
+	@RequestMapping(value="/GosuInsert")
+	public ModelAndView insert(GosuVo vo, HttpServletRequest request) {
+		HttpSession session=request.getSession();
+		vo.setEmail((String)session.getAttribute("email"));
+		vo.setGrade("초수");
+		vo.setEmploy(0);
+		//vo.setImage("0");
+		int n=gosuService.insert(vo);
+		ModelAndView mv=new ModelAndView("woohyun/result");
+		if(n>0) {
+			mv.addObject("code","success");
+			session.setAttribute("gosuYN", vo.getEmail());	// 고수 등록하면서 바로 세션에 고수로 등록 함
+			session.setAttribute("flag", 2);	// 고수 등록하면서 바로 세션에 flag 2:고수 등록 함
+		}else {
+			mv.addObject("code","fail");
+		}
+		return mv;
+	}
+	
+}
--- guemgo_project/src/main/webapp/WEB-INF/views/woohyun/pointCharge.jsp
+++ guemgo_project/src/main/webapp/WEB-INF/views/woohyun/pointCharge.jsp
@@ -8,35 +8,27 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>pointCharge.jsp</title>
 </head>
 <body>
 <h1>포인트 충전</h1>
-<table border=3 align="center">
-	${vo.email } 님의 포인트 : ${vo.point } 점
-</table>
-<table border=3 align="center">
-	<tr>
-		포인트를 얼마나 충전하시겠습니까?
-	</tr>
-	<tr>
+	<p>${vo.email } 님의 포인트 : ${vo.point } 점
+	<p>포인트를 얼마나 충전하시겠습니까?
+<form>
 		<ul>
-			<li>100포인트	1,000원(0%)</li>
-			<li>300포인트	2,700원(10%)</li>
-			<li>500포인트	4,000원(20%)</li>
-			<li>1000포인트	7,000원(30%)</li>
+			<li><input type="radio" name="point" value="100" onclick="" checked />100포인트	1,000원(0%)</li>
+			<li><input type="radio" name="point" value="300" onclick=""  />300포인트	2,700원(10%)</li>
+			<li><input type="radio" name="point" value="500" onclick=""  />500포인트	4,000원(20%)</li>
+			<li><input type="radio" name="point" value="1000" onclick=""  />1000포인트	7,000원(30%)</li>
 		</ul>
-	</tr>
-</table>
-<table border=3 align="center">
 <h2>결제정보를 입력해주세요</h2>
-카드번호<input type="text"><br>
-유효기간<input type="text"><br>
-생년월일(6자리)<input type="text"><br>
-카드비밀번호 앞 2자리<input type="text"><br>
+	카드번호<input type="text"><br>
+	유효기간<input type="text"><br>
+	생년월일(6자리)<input type="text"><br>
+	카드비밀번호 앞 2자리<input type="text"><br>
 <input type="submit" name="payment" value="결제">
+</form>
 
 <p><a href="<c:url value='/'/>">홈으로
-</table>
 </body>
 </html>
